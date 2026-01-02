5bb6e6928a9be8ae3f1c527fb4dacde7bfd79f95
ashok9109 <aaaaashokyadav123@gmail.com>
2026-01-02 15:27:45 +0530

    Add contact page with form and styles; implement success message functionality

--- script.js
+++ script.js
@@ -7,3 +7,32 @@ toggle.addEventListener('click', () => {
     navlink.classList.toggle('active');
 });
 
+// booking form submit
+
+const form = document.getElementById("bookingForm");
+const successmsg = document.getElementById("successmsg");
+console.log("this form", form)
+
+if(form){
+
+    form.addEventListener('submit', function(e){
+        e.preventDefault();
+
+        successmsg.textContent = "Your table is booked successfully";
+        form.reset()
+    });
+};
+
+// contact form submit code
+
+const contactForm = document.getElementById("contactForm");
+const contactSuccess = document.getElementById("contactSuccess");
+
+if(contactForm){
+
+    contactForm.addEventListener("submit", function(e){
+        e.preventDefault()
+        contactSuccess.textContent = 'Message send successfully'
+        contactForm.reset()
+    });
+};
